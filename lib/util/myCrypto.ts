9fe04f2b70a9c711cc78032c00ae07cabe4300cc
ChenxiYuan <yuanchenxi95@gmail.com>
2018-06-28 15:04:54 +0800

    change md5 to lowercase

--- lib/util/myCrypto.ts
+++ lib/util/myCrypto.ts
@@ -1,9 +1,10 @@
 import Base64 from 'crypto-js/enc-base64'
-import MD5 from 'crypto-js/MD5'
+import md5 from 'crypto-js/md5'
 import hmacSHA256 from 'crypto-js/hmac-sha256'
 
+
 export function myMd5 (content: string): string {
-  return Base64.stringify(MD5(content))
+  return Base64.stringify(md5(content))
 }
 
 export function myHmacsha256 (content: string, appSecret: string): string {
