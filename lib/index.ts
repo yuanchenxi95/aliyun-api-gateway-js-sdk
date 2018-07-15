1eff15c878e41d01c967d402fac9f8391aff3ffa
ChenxiYuan <yuanchenxi95@gmail.com>
2018-07-15 15:33:29 +0800

    fix client typo

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-import { Client } from './Client'
+import { Client } from './client'
 import { SimpleClient } from './simple-client'
 import * as ContentTypes from './constants/contentTypes'
 export { Client, SimpleClient, ContentTypes }
